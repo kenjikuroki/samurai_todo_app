02195bdaa5337ecd20481ea5e81c708a08e82c50
kenjikuroki <90098303+kenjikuroki@users.noreply.github.com>
2022-01-10 15:36:37 +0900

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::resource("goals", "GoalController");
+
+Route::resource("goals.todos", "TodoController");
+
+Route::post('/goals/{goal}/todos/{todo}/sort', 'TodoController@sort');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
